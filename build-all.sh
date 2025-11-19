10772c9ce334cea3f752872744f47dcd61866055
Alex Rønne Petersen <alex@alexrp.com>
2025-11-19 08:52:01 +0100

    build-all.sh: update list of targets

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,14 @@
 set -e
+
+CMAKE_GENERATOR=Ninja ./build x86_64-freebsd-none baseline
+
+CMAKE_GENERATOR=Ninja ./build aarch64-linux-musl baseline
+CMAKE_GENERATOR=Ninja ./build loongarch64-linux-musl baseline
+CMAKE_GENERATOR=Ninja ./build riscv64-linux-musl baseline
+CMAKE_GENERATOR=Ninja ./build s390x-linux-musl baseline
+CMAKE_GENERATOR=Ninja ./build x86_64-linux-musl baseline
+
+CMAKE_GENERATOR=Ninja ./build aarch64-macos-none baseline
+
 CMAKE_GENERATOR=Ninja ./build aarch64-windows-gnu baseline
 CMAKE_GENERATOR=Ninja ./build x86_64-windows-gnu baseline
-CMAKE_GENERATOR=Ninja ./build x86_64-macos-none baseline
-CMAKE_GENERATOR=Ninja ./build x86_64-linux-musl baseline
-CMAKE_GENERATOR=Ninja ./build aarch64-macos-none apple_a14
-CMAKE_GENERATOR=Ninja ./build aarch64-linux-musl baseline
